8d681f8bef598ce4e29fb768127fbf72f6af70b4
Adi Skop <adiskop@gmail.com>
2020-07-10 14:02:38 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -22,7 +22,7 @@ def bonus
 
   #code your solution here:
 hero["Romeo"][:status]="alive"
-
+heroine["Romeo"][:status]="alive"
 
   #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
   epic_tragedy
